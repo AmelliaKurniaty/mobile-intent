e9a4fccf3ddcbb8f8f3d454be0903f6817e457d8
AmelliaKurniaty <56011358+AmelliaKurniaty@users.noreply.github.com>
2020-02-18 07:43:18 +0700

    praktikum Explicit Intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,4 @@ public class ExplicitIntentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
     }
-
 }
